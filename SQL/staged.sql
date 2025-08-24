0766109d8a1b8e7a9273d31951fed637bfa66e25
Michael Galo <michael@michaelthinks.co>
2025-08-24 16:54:27 -0500

    staging sql completed

--- SQL/staged.sql
+++ SQL/staged.sql
@@ -13,4 +13,35 @@ SELECT
     spacewalks_count,
     url,
     thumbnail_url
-FROM RAW.ASTRONAUTS
+FROM RAW.ASTRONAUTS;
+
+CREATE TABLE IF NOT EXISTS STAGED.NASA_APOD AS
+SELECT
+    date,
+    title,
+    explanation,
+    url,
+    hdurl
+FROM RAW.NASA_APOD;
+
+CREATE TABLE IF NOT EXISTS STAGED.NASA_DONKI AS
+SELECT
+    messageID,
+    messageType,
+    messageIssueTime,
+    messageBody
+FROM RAW.NASA_DONKI;
+
+CREATE TABLE IF NOT EXISTS STAGED.NASA_EXOPLANETS AS
+SELECT
+    pl_name AS planet_name,
+    disc_year AS discovery_year,
+    pl_controv_flag AS controversial_flag,
+    discoverymethod AS discovery_method,
+    disc_facility AS discovery_facility,
+    disc_instrument AS discovery_instrument,
+    pl_orbper AS orbital_period_days,
+    pl_rade AS radius_earth_radii,
+    st_rad AS star_radius_solar_radii,
+    pl_orbsmax AS orbital_semi_major_axis_in_au -- the average distance the star and planet are apart in astronomical units (how far the sun is from earth)
+FROM RAW.NASA_EXOPLANETS;
