d45653dcbb78b3c4e3b0b606e54c0360e7ad460a
Michael Galo <michael@michaelthinks.co>
2025-09-05 18:57:12 -0500

    replaced wildcard for APOD SQL scripts to specific columns

--- SQL/staging/STAGED_NASA_APOD.sql
+++ SQL/staging/STAGED_NASA_APOD.sql
@@ -1,4 +1,14 @@
 CREATE OR REPLACE TABLE STAGED.NASA_APOD AS
 SELECT
-    * -- intentionally using wildcard due to daily changing columns schema
-FROM RAW.NASA_APOD;
+    title,
+    explanation,
+    date,
+    url,
+    hdurl,
+    thumbnail_url,
+    copyright
+FROM RAW.NASA_APOD
+WHERE date IS NOT NULL
+    AND title IS NOT NULL
+    AND url IS NOT NULL
+    AND explanation IS NOT NULL;
